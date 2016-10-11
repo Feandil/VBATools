1fbeaa0982065def5a134dfb0aaf5ce61943e753
Vincent Brillault <vincent.brillault@cern.ch>
2016-10-11 15:15:25 +0200

    Move VBA parsing to a sub folder

--- VBADeobsfuscator.py
+++ VBADeobsfuscator.py
@@ -1,18 +1,9 @@
 from oletools.olevba import VBA_Parser
 from antlr4 import *
-from VBALexer import VBALexer
-from VBAParser import VBAParser
+from parser import VBA
+from extractor import Extractor
 
 class VBADeobsfuscator(object):
-
-    VAR_DECL_XPATH = ['ModuleSimpleContext', 'ModuleDeclarationsContext',
-                      'ModuleDeclarationsElementContext', 'VariableStmtContext']
-    VAR_IDENT_XPATH = ['VariableListStmtContext', 'VariableSubStmtContext', 'IdentifierContext']
-    SUB_DECL_XPATH = ['ModuleSimpleContext', 'ModuleBodyContext', 'ModuleBodyElementContext', 'SubStmtContext']
-    SUB_IDENT_XPATH = ['SubroutineNameContext', 'IdentifierContext', '*', 'IdentifierValueContext']
-    FUN_DECL_XPATH = ['ModuleSimpleContext', 'ModuleBodyContext', 'ModuleBodyElementContext', 'FunctionStmtContext']
-    FUN_IDENT_XPATH = ['FunctionNameContext', 'IdentifierContext', '*', 'IdentifierValueContext']
-    ARG_XPATH = ['ArgListContext', 'ArgContext', '*', 'IdentifierContext', '*', 'IdentifierValueContext']
 
     def __init__(self, file):
         vbaparser = VBA_Parser(file)
@@ -22,67 +13,22 @@ class VBADeobsfuscator(object):
         self._fun = {}
         self._sub = {}
 
-    @staticmethod
-    def _xpath(node, path):
-        if isinstance(node, tree.Tree.TerminalNodeImpl):
-            return []
-        if not path:
-            return [node]
-        if not node.getChildCount():
-            return []
-        name = path[0]
-        path = path[1:]
-        if name == '*':
-            return [ret for elt in node.children for ret in VBADeobsfuscator._xpath(elt, path)]
-        cls = getattr(VBAParser, name)
-        ret = []
-        for child in node.children:
-            if isinstance(child, cls):
-                ret.extend(VBADeobsfuscator._xpath(child, path))
-        return ret
-
-    def _extract_part(self, name):
-        decl_xpath = getattr(self, '{0}_DECL_XPATH'.format(name.upper()))
-        ident_xpath = getattr(self, '{0}_IDENT_XPATH'.format(name.upper()))
-        dic = getattr(self, '_{0}'.format(name.lower()))
-        for content in self._content:
-            for elt in VBADeobsfuscator._xpath(content, decl_xpath):
-                names = VBADeobsfuscator._xpath(elt, ident_xpath)
-                assert(len(names) == 1)
-                dic[names[0].getText()] = elt
 
     def parse(self):
         for vba in self._raw:
-            lexer = VBALexer(InputStream(vba))
-            stream = CommonTokenStream(lexer)
-            parser = VBAParser(stream)
-            content = parser.startRuleSimple()
-            assert(isinstance(content, VBAParser.StartRuleSimpleContext))
-            assert(isinstance(content.children[0], VBAParser.ModuleSimpleContext))
+            content = VBA.parse(vba)
             self._content.append(content)
-        self._extract_part('var')
-        self._extract_part('sub')
-        self._extract_part('fun')
-
-    @staticmethod
-    def _findall(node, cls):
-        if isinstance(node, tree.Tree.TerminalNodeImpl):
-            return []
-        if not node.getChildCount():
-            return []
-        if isinstance(cls, basestring):
-            cls = getattr(VBAParser, cls)
-        if isinstance(node, cls):
-            return [node]
-        return [e for c in node.children for e in VBADeobsfuscator._findall(c, cls)]
+            self._var.update(VBA.extract_var(content))
+            self._fun.update(VBA.extract_fun(content))
+            self._sub.update(VBA.extract_sub(content))
 
     @staticmethod
     def _check_arguments(name, fun):
-      raw_arguments = VBADeobsfuscator._xpath(fun, VBADeobsfuscator.ARG_XPATH)
+      raw_arguments = VBA.xpath(fun, VBA.ARG_XPATH)
       text_args = [e.getText() for e in raw_arguments]
-      blocks = VBADeobsfuscator._xpath(fun, ['BlockContext'])
+      blocks = VBA.xpath(fun, ['BlockContext'])
       assert(len(blocks) == 1)
-      raw_identifiers = set(VBADeobsfuscator._findall(blocks[0], 'IdentifierValueContext'))
+      raw_identifiers = set(VBA.findall(blocks[0], 'IdentifierValueContext'))
       text_ids = [e.getText() for e in raw_identifiers]
       for arg in text_args:
           if arg not in text_ids:
@@ -93,3 +39,11 @@ class VBADeobsfuscator(object):
            self._check_arguments(fun, self._fun[fun])
        for sub in self._sub:
            self._check_arguments(sub, self._sub[sub])
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) != 2:
+        print('Expected 1 argument: file to analyse')
+        sys.exit(-1)
+    d = VBADeobsfuscator(sys.argv[1])
+    d.parse()
---
+++ parser/__init__.py
@@ -0,0 +1,1 @@
+from .vba import VBA
