7c954bd00d207cfa66a4b2799e5a334e1868e7db
Vincent Brillault <vincent.brillault@cern.ch>
2016-10-19 09:43:37 +0200

    Add new basic de-obfuscation script
    - Rename all fun/sub except the one which are not internally called
    - Rename all internal variable
    The output should reproducible.
    
    When applied to a Dridex wave, while all payload have a different VBA,
    all the de-obfuscated VBA have the same hash :)

---
+++ deobfuscator.py
@@ -0,0 +1,123 @@
+import string
+
+from parser import Parser
+
+class Deobsfuscator(Parser):
+
+    def __init__(self, file):
+        super(Deobsfuscator, self).__init__(file)
+        self._index = -1
+        self._known_identifier = None
+        self._identifier_order = []
+        self._deps = {}
+        self._var = {}
+        self._proc = {}
+        self._populate_vars()
+        self._populate_proc()
+        self._build_dependency_tree()
+
+    def _populate_vars(self):
+        for varst in self.findall(self.decl, 'variableSubStmt'):
+            name = self.identifier_name(varst)
+            self._var[name] = varst
+            self._identifier_order.append(name)
+
+    def _populate_proc(self):
+        for subst in self.findall(self.body, 'subStmt'):
+            name = self.identifier_name(subst)
+            self._proc[name] = subst
+            self._identifier_order.append(name)
+        for funst in self.findall(self.body, 'functionStmt'):
+            name = self.identifier_name(funst)
+            self._proc[name] = funst
+            self._identifier_order.append(name)
+
+    def _populate_identifiers(self):
+        if self._known_identifier is None:
+            self._known_identifier = set()
+            for identifier in self.findall(self.decl, 'IDENTIFIER'):
+                self._known_identifier.add(identifier['value'])
+            for identifier in self.findall(self.body, 'IDENTIFIER'):
+                self._known_identifier.add(identifier['value'])
+
+    def _build_dependency_tree(self):
+        self._deps = {}
+        for proc in self._proc:
+            for proccall in self.findall(self._proc[proc], 'iCS_B_ProcedureCall'):
+                name = self.identifier_name(proccall)
+                try:
+                    self._deps[name].add(proc)
+                except KeyError:
+                    self._deps[name] = set([proc])
+            for procvar in self.findall(self._proc[proc], 'iCS_S_VariableOrProcedureCall'):
+                name = self.identifier_name(procvar)
+                try:
+                    self._deps[name].add(proc)
+                except KeyError:
+                    self._deps[name] = set([proc])
+
+    def _next_name(self):
+        self._index +=1
+        if (self._index < len(string.ascii_letters)):
+            return '_{0}_'.format(string.ascii_letters[self._index])
+        if (self._index < len(string.ascii_letters) * (len(string.ascii_letters) + 1)):
+            return '_{0}{1}_'.format(string.ascii_letters[self._index / len(string.ascii_letters) - 1],
+                                     string.ascii_letters[self._index % len(string.ascii_letters)])
+        sys.exit(-1)
+
+    def _next_valid_name(self):
+        self._populate_identifiers()
+        name = self._next_name()
+        while name in self._known_identifier:
+            name = self._next_name()
+        self._known_identifier.add(name)
+        return name
+
+    def _rename(self, node, oldname, newname):
+        for identifier in self.findall(node, 'IDENTIFIER'):
+            if identifier['value'] == oldname:
+                identifier['value'] = newname
+
+    def rename(self, oldname, node=None):
+        newname = self._next_valid_name()
+        if node is None:
+            self._rename(self.decl, oldname, newname)
+            self._rename(self.body, oldname, newname)
+            self._known_identifier.remove(oldname)
+        else:
+            self._rename(node, oldname, newname)
+
+    def clean_proc_variables(self, proc_name):
+        proc = self._proc[proc_name]
+        for argument in self.xpath(proc,['argList', 'arg']):
+            name = self.identifier_name(argument)
+            self.rename(name, proc)
+        for node_type in ['lineLabel', 'forEachStmt', 'forNextStmt']:
+            for stmt in self.findall(proc, node_type):
+                name = self.identifier_name(stmt)
+                if name != proc_name and name not in self._identifier_order:
+                    self.rename(name, proc)
+        for node_type in ['setStmt', 'letStmt']:
+            for setstmt in self.findall(proc, node_type):
+                var = self.xpath(setstmt, ['implicitCallStmt_InStmt', '*'])[0]
+                name = self.identifier_name(var)
+                if name != proc_name and name not in self._identifier_order:
+                    self.rename(name, proc)
+
+    def clean_ids(self):
+        for id in self._identifier_order:
+           if id in self._proc:
+               self.clean_proc_variables(id)
+        for id in self._identifier_order:
+           if id in d._deps:
+               self.rename(id)
+
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) != 2:
+        print('Expected 1 argument: file to analyse')
+        sys.exit(-1)
+    d = Deobsfuscator(sys.argv[1])
+    d.clean_ids()
+    print(d.get_text())
